38df33c550fee199253ce5908ffaa20548f5d171
Emma <zy_sunday@163.com>
2016-10-02 23:01:41 +0800

    Add ios files

---
+++ ios/RCTInstabug/RCTInstabug.h
@@ -0,0 +1,13 @@
+//
+//  RCTInstabug.h
+//  RCTInstabug
+//
+//  Created by Kenny on 16/10/2.
+//  Copyright © 2016年 react-native-instabug. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTInstabug : NSObject
+
+@end
---
+++ ios/RCTInstabug/RCTInstabug.m
@@ -0,0 +1,13 @@
+//
+//  RCTInstabug.m
+//  RCTInstabug
+//
+//  Created by Kenny on 16/10/2.
+//  Copyright © 2016年 react-native-instabug. All rights reserved.
+//
+
+#import "RCTInstabug.h"
+
+@implementation RCTInstabug
+
+@end
