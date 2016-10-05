5aecb8fb1f27f3d38c00898435e1b26a77da8d5c
Kennytian <kenny_tian@outlook.com>
2016-10-05 19:20:11 +0800

    Add method for iOS

--- ios/RCTInstabug/RCTInstabug.m
+++ ios/RCTInstabug/RCTInstabug.m
@@ -1,11 +1,48 @@
 #import "RCTInstabug.h"
+#import "RCTConvert.h"
 
 @implementation RCTInstabug
 
-- (dispatch_queue_t)(methodQueue)
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(report:(NSString *)flag)
 {
-    return dispatch_get_main_queue();
+    RCTLogInfo(@"You input result is %@", flag);
 }
-RTC_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(addTags:(NSString *)tags)
+{
+    RCTLogInfo(@"You input result is %@", tags);
+}
+
+RCT_EXPORT_METHOD(changeLocale:(NSString *)languageTag)
+{
+    RCTLogInfo(@"You input result is %@", languageTag);
+}
+
+RCT_EXPORT_METHOD(setFileAttachment:(NSString *)fileUri fileNameWithExtension:(NSString *)fileNameWithExtension)
+{
+    RCTLogInfo(@"You input result is %@ and %@", fileUri, fileNameWithExtension);
+}
+
+RCT_EXPORT_METHOD(setUserEmail:(NSString *)userEmail)
+{
+    RCTLogInfo(@"You input result is %@", userEmail);
+}
+
+RCT_EXPORT_METHOD(setUsername:(NSString *)username)
+{
+    RCTLogInfo(@"You input result is %@", username);
+}
+
+RCT_EXPORT_METHOD(setUserData:(NSString *)userData)
+{
+    RCTLogInfo(@"You input result is %@", userData);
+}
+
+RCT_EXPORT_METHOD(showIntroMessage)
+{
+    RCTLogInfo(@"showIntroMessage");
+}
 
 @end
