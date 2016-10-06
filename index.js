77ff828277d9952112821a1af3b4fb74c9cd52e3
Prithvi Sharma <prithsharma100@gmail.com>
2016-10-06 11:05:58 +0530

    Convert tabs to spaces

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ import {NativeModules} from 'react-native';
 let {Instabug} = NativeModules;
 
 module.exports = {
-	reportFeedback: function () {
-		Instabug.report('feedback');
-	},
+  reportFeedback: function () {
+    Instabug.report('feedback');
+  },
 
-	reportBug: function () {
-		Instabug.report('bug');
-	},
+  reportBug: function () {
+    Instabug.report('bug');
+  },
 
   /**
    * Show Instabug dialog so user can choose to report a bug, or submit feedback
@@ -20,79 +20,79 @@ module.exports = {
     Instabug.report('');
   },
 
-	/**
-	 * Adds tag(s) to issues before sending them
-	 * @param tags  NOTICE: multiple tags with comma(,) split
-	 */
-	addTags: function (tags) {
-		Instabug.addTags(tags);
-	},
+  /**
+   * Adds tag(s) to issues before sending them
+   * @param tags  NOTICE: multiple tags with comma(,) split
+   */
+  addTags: function (tags) {
+    Instabug.addTags(tags);
+  },
 
-	/**
-	 * None -> IBGInvocationEventNone
-	 * TwoFingersSwipeLeft -> IBGInvocationEventTwoFingersSwipeLeft
-	 * FloatingButton -> IBGInvocationEventFloatingButton
-	 * Shake -> IBGInvocationEventShake
-	 * ScreenshotGesture -> IBGInvocationScreenshotGesture
-	 *
-	 Changes the event used to invoke Instabug SDK
-	 * @param eventTag
-	 */
-	changeInvocationEvent: function (eventTag) {
-		Instabug.changeInvocationEvent(eventTag);
-	},
+  /**
+   * None -> IBGInvocationEventNone
+   * TwoFingersSwipeLeft -> IBGInvocationEventTwoFingersSwipeLeft
+   * FloatingButton -> IBGInvocationEventFloatingButton
+   * Shake -> IBGInvocationEventShake
+   * ScreenshotGesture -> IBGInvocationScreenshotGesture
+   *
+   Changes the event used to invoke Instabug SDK
+   * @param eventTag
+   */
+  changeInvocationEvent: function (eventTag) {
+    Instabug.changeInvocationEvent(eventTag);
+  },
 
-	/**
-	 [CHINA/CHINESE/PRC/SIMPLIFIED_CHINESE -> CHINESE]
-	 [TAIWAN/TRADITIONAL_CHINESE -> TAIWAN]
-	 [ENGLISH -> ENGLISH]
-	 [UK -> UK]
-	 [US -> US]
-	 * @param languageTag
-	 */
-	changeLocale: function (languageTag) {
-		Instabug.changeLocale(languageTag);
-	},
+  /**
+   [CHINA/CHINESE/PRC/SIMPLIFIED_CHINESE -> CHINESE]
+   [TAIWAN/TRADITIONAL_CHINESE -> TAIWAN]
+   [ENGLISH -> ENGLISH]
+   [UK -> UK]
+   [US -> US]
+   * @param languageTag
+   */
+  changeLocale: function (languageTag) {
+    Instabug.changeLocale(languageTag);
+  },
 
-	/**
-	 * The file at filePath will be uploaded along upcoming reports with the name fileNameWithExtension
-	 * @param fileUri
-	 * @param fileNameWithExtension
-	 */
-	setFileAttachment: function (fileUri, fileNameWithExtension) {
-		Instabug.setFileAttachment(fileUri, fileNameWithExtension);
-	},
+  /**
+   * The file at filePath will be uploaded along upcoming reports with the name fileNameWithExtension
+   * @param fileUri
+   * @param fileNameWithExtension
+   */
+  setFileAttachment: function (fileUri, fileNameWithExtension) {
+    Instabug.setFileAttachment(fileUri, fileNameWithExtension);
+  },
 
-	/**
-	 * If your app already acquires the user's email address and you provide it to this method,
-	 * Instabug will pre-fill the user email in reports.
-	 * @param userEmail
-	 */
-	setUserEmail: function (userEmail) {
-		Instabug.setUserEmail(userEmail);
-	},
+  /**
+   * If your app already acquires the user's email address and you provide it to this method,
+   * Instabug will pre-fill the user email in reports.
+   * @param userEmail
+   */
+  setUserEmail: function (userEmail) {
+    Instabug.setUserEmail(userEmail);
+  },
 
-	/**
-	 * Sets the user name that is used in the dashboard's contacts.
-	 * @param username
-	 */
-	setUsername: function (username) {
-		Instabug.setUsername(username);
-	},
+  /**
+   * Sets the user name that is used in the dashboard's contacts.
+   * @param username
+   */
+  setUsername: function (username) {
+    Instabug.setUsername(username);
+  },
 
-	/**
-	 * Adds specific user data that you need to be added to the reports
-	 * @param userData
-	 */
-	setUserData: function (userData) {
-		Instabug.setUserData(userData);
-	},
+  /**
+   * Adds specific user data that you need to be added to the reports
+   * @param userData
+   */
+  setUserData: function (userData) {
+    Instabug.setUserData(userData);
+  },
 
-	/**
-	 * Call this method to display the discovery dialog explaining the shake gesture or the two finger swipe gesture,
-	 * if you've enabled it   i.e: This method is automatically called on first run of the application
-	 */
-	showIntroMessage: function () {
-		Instabug.showIntroMessage();
-	},
+  /**
+   * Call this method to display the discovery dialog explaining the shake gesture or the two finger swipe gesture,
+   * if you've enabled it   i.e: This method is automatically called on first run of the application
+   */
+  showIntroMessage: function () {
+    Instabug.showIntroMessage();
+  },
 };
